2968a33d757f12ec2aa3e1dd0c01cac28ba1afce
yamanono <yamaday712@gmail.com>
2023-08-10 23:22:02 +0900

    変更した。

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ button.addEventListener("click", play);
 
 function play() {
   var rythm = new Rythm();
-  rythm.setMusic("samples/A.mp3");
+  rythm.setMusic("samples/rythmCß.mp3");
   rythm.start();
   console.log("play");
 }
